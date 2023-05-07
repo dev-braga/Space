5c2aa43e2188a1bc9ca1b462624cc4b78c39992a
dev-braga <braga.trader12@gmail.com>
2023-05-07 11:53:03 -0300

    Feito atualização para o mobile e tablet

--- js/menuNav.js
+++ js/menuNav.js
@@ -5,6 +5,18 @@ let url3 = "url('assets/crew/background-crew-desktop.jpg')"
 let url4 = "url('assets/technology/background-technology-desktop.jpg')"
 let background = document.querySelector('.header')
 
+// Background tablet =====
+let urlTablet1 = "url('assets/home/background-home-tablet.jpg')"
+let urlTablet2 = "url('assets/destination/background-destination-tablet.jpg')"
+let urlTablet3 = "url('assets/crew/background-crew-tablet.jpg')"
+let urlTablet4 = "url('assets/technology/background-technology-tablet.jpg')"
+
+// Background tablet =====
+let urlMobile1 = "url('assets/home/background-home-mobile.jpg')"
+let urlMobile2 = "url('assets/destination/background-destination-mobile.jpg')"
+let urlMobile3 = "url('assets/crew/background-crew-mobile.jpg')"
+let urlMobile4 = "url('assets/technology/background-technology-mobile.jpg')"
+
 let item1 = document.querySelector(".container-border1")
 let item2 = document.querySelector(".container-border2")
 let item3 = document.querySelector(".container-border3")
@@ -16,7 +28,16 @@ let tab3Active = document.querySelector("#crew")
 let tab4Active = document.querySelector("#technology")
 
 // obtém todos os elementos radio button do grupo "meuGrupo"
-var radios = document.getElementsByName("radio-check");
+let radios = document.getElementsByName("radio-check");
+
+//Variaveis para controlar qual tela esta sendo acessada
+let isHome = false
+let isDestination = false
+let isCrew = false
+let isTechnology = false
+// Variavel de verificacao do aparelho utilizado
+let isMobile = 600
+let isTablet = 900
 
 // adiciona um evento de clique a cada um desses elementos
 for (var i = 0; i < radios.length; i++) {
@@ -38,6 +59,11 @@ for (var i = 0; i < radios.length; i++) {
         tab3Active.style.display = "none";
         tab4Active.style.display = "none";
         fecharMenu()
+        mudarImage1()
+        isHome = true
+        isDestination = false
+        isCrew = false
+        isTechnology = false
     }
     else if(radios[1].checked){
         item2.style.background = "#fff"
@@ -50,6 +76,10 @@ for (var i = 0; i < radios.length; i++) {
         tab3Active.style.display = "none";
         tab4Active.style.display = "none";
         fecharMenu()
+        isHome = false
+        isDestination = true
+        isCrew = false
+        isTechnology = false
     }
     else if(radios[2].checked){
         item3.style.background = "#fff"
@@ -57,12 +87,15 @@ for (var i = 0; i < radios.length; i++) {
         item1.style.background = ""
         item4.style.background = ""
     
-
         tabActive.style.display = "none";
         tab2Active.style.display = "none";
         tab3Active.style.display = "block";
         tab4Active.style.display = "none";
         fecharMenu()
+        isHome = false
+        isDestination = false
+        isCrew = true
+        isTechnology = false
         
     }
     else if(radios[3].checked){
@@ -76,6 +109,10 @@ for (var i = 0; i < radios.length; i++) {
         tab3Active.style.display = "none";
         tab4Active.style.display = "block";
         fecharMenu()
+        isHome = false
+        isDestination = false
+        isCrew = false
+        isTechnology = true
     }
     
   });
@@ -87,65 +124,182 @@ function fecharMenu(){
 }
 
 function mudarImage1(){
+    let wWidth = window.innerWidth
+
     background.style.backgroundImage = url1
     background.style.backgroundRepeat = "no-repeat"
     background.style.backgroundSize = "cover"
-    
-    //item1.style.background = "#fff"
-    // Remover a aba dos outros links
-    //item2.classList.remove("border-bottom-custom")
-   // item3.classList.remove("border-bottom-custom")
-    //item4.classList.remove("border-bottom-custom")
-
-    // Remover classe padrao
+    isHome = true
     tabActive.classList.remove("active")
+    // Inserir background mobile caso seja true
+    if(wWidth < isMobile){
+        background.style.backgroundImage = urlMobile1
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
+    // Inserir background tablet caso seja true
+    if(wWidth > isMobile && wWidth < isTablet){
+        background.style.backgroundImage = urlTablet1
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
 }
 function mudarImage2(){
+    let wWidth = window.innerWidth
     background.style.backgroundImage = url2
     background.style.backgroundRepeat = "no-repeat"
     background.style.backgroundSize = "cover"
+    isDestination = true
 
-    //item2.classList.add("border-bottom-custom")
+    // Inserir background mobile caso seja true
+    if(wWidth < isMobile){
+        background.style.backgroundImage = urlMobile2
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
+    // Inserir background tablet caso seja true
+    if(wWidth > isMobile && wWidth < isTablet){
+        background.style.backgroundImage = urlTablet2
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
 
-    // Remover a aba dos outros links
-    //item1.classList.remove("border-bottom-custom")
-    //item3.classList.remove("border-bottom-custom")
-    //item4.classList.remove("border-bottom-custom")
-    // Remover classe padrao
-    //tabActive.classList.remove("active")
 }
 function mudarImage3(){
+    let wWidth = window.innerWidth
     background.style.backgroundImage = url3
     background.style.backgroundRepeat = "no-repeat"
     background.style.backgroundSize = "cover"
-
-    //tem3.classList.add("border-bottom-custom")
-
-    // Remover a aba dos outros links
-   // item1.classList.remove("border-bottom-custom")
-    //item2.classList.remove("border-bottom-custom")
-    //item4.classList.remove("border-bottom-custom")
-    // Remover classe padrao
-   // tabActive.classList.remove("active")
+    isCrew = true 
+    
+    // Inserir background mobile caso seja true
+    if(wWidth < isMobile){
+        background.style.backgroundImage = urlMobile3
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
+    // Inserir background tablet caso seja true
+    if(wWidth > isMobile && wWidth < isTablet){
+        background.style.backgroundImage = urlTablet3
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
 }
 function mudarImage4(){
+    let wWidth = window.innerWidth
     background.style.backgroundImage = url4
     background.style.backgroundRepeat = "no-repeat"
     background.style.backgroundSize = "cover"
     background.style.backgroundClip = "content-box"
-
-    //item4.classList.add("border-bottom-custom")
-
-    // Remover a aba dos outros links
-    //item1.classList.remove("border-bottom-custom")
-    //item2.classList.remove("border-bottom-custom")
-    //item3.classList.remove("border-bottom-custom")
-    // Remover classe padrao
-
+    
+    // Inserir background mobile caso seja true
+    if(wWidth < isMobile){
+        background.style.backgroundImage = urlMobile4
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
+    // Inserir background tablet caso seja true
+    if(wWidth > isMobile && wWidth < isTablet){
+        background.style.backgroundImage = urlTablet4
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
+    }
 }
 
 window.addEventListener('load',  ()=>{
     tabActive.style.display = "block";
     item1.style.background = "#fff"
+
     mudarImage1()
+    isHome = true
 })
+
+window.addEventListener('resize', function() {
+    verifyResize()    
+});
+
+verifyResize=()=>{
+    if(window.innerWidth < isMobile ){
+        // Verificando Mobile Backgrounds =========
+        if( isHome ){
+            background.style.backgroundImage = urlMobile1
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+    
+        }
+        else if( isDestination ){
+            background.style.backgroundImage = urlMobile2
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+           
+        }
+        else if( isCrew ){
+            background.style.backgroundImage = urlMobile3
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+            
+        }
+        else if( isTechnology ){
+            background.style.backgroundImage = urlMobile4
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+            
+        }
+    }
+    // Verificando Tablet Backgrounds =========
+    if(window.innerWidth > isMobile && window.innerWidth < isTablet){
+        // Verificando Mobile Backgrounds =========
+        if( isHome ){
+            background.style.backgroundImage = urlTablet1
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+    
+        }
+        else if( isDestination ){
+            background.style.backgroundImage = urlTablet2
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+           
+        }
+        else if( isCrew ){
+            background.style.backgroundImage = urlTablet3
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+            
+        }
+        else if( isTechnology ){
+            background.style.backgroundImage = urlTablet4
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+            
+        }
+    }
+    // Verificando Desktop Backgrounds =========
+    if(window.innerWidth > isTablet){
+        // Verificando Mobile Backgrounds =========
+        if( isHome ){
+            background.style.backgroundImage = url1
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+    
+        }
+        else if( isDestination ){
+            background.style.backgroundImage = url2
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+           
+        }
+        else if( isCrew ){
+            background.style.backgroundImage = url3
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+            
+        }
+        else if( isTechnology ){
+            background.style.backgroundImage = url4
+            background.style.backgroundRepeat = "no-repeat"
+            background.style.backgroundSize = "cover"
+            
+        }
+    }
+}
--- js/tech.js
+++ js/tech.js
@@ -4,6 +4,13 @@ const slides = document.querySelectorAll('.sliderTech-slide');
 const image1 = document.querySelector("#image-tech1")
 const image2 = document.querySelector("#image-tech2")
 const image3 = document.querySelector("#image-tech3")
+const image1l = document.querySelector("#image-tech1l")
+const image2l = document.querySelector("#image-tech2l")
+const image3l = document.querySelector("#image-tech3l")
+
+let slide1 = false
+let slide2 = false
+let slide3 = false
 
 // Função para mostrar um slide e atualizar o botão ativo
 function showSlide(n) {
@@ -11,21 +18,61 @@ function showSlide(n) {
   slides.forEach(slide => slide.classList.remove('active'));
   buttons[n].classList.add('active');
   slides[n].classList.add('active');
-
+  let wWidth = window.innerWidth
+  
   if(n == 0){
     image1.style.display = "block"
     image2.style.display = "none"
     image3.style.display = "none"
+    
+    if (wWidth <= isTablet ){ // isTablet eh uma variavel definida com 900 pixels
+      image1l.style.display = "block"
+      image2l.style.display = "none"
+      image3l.style.display = "none"
+      // Imagens no modo portrait
+      image1.style.display = "none"
+      image2.style.display = "none"
+      image3.style.display = "none"
+      }
+    slide1 = true
+    slide2 = false
+    slide3 = false
   }
   else if( n == 1){
     image2.style.display = "block"
     image1.style.display = "none"
     image3.style.display = "none"
+    if( wWidth <= isTablet ){
+      image1l.style.display = "none"
+      image2l.style.display = "block"
+      image3l.style.display = "none"
+      // Imagens no modo portrait
+      image2.style.display = "none"
+      image1.style.display = "none"
+      image3.style.display = "none"
+      
+    }
+    slide1 = false
+    slide2 = true
+    slide3 = false
   }
   else{
     image3.style.display = "block"
     image1.style.display = "none"
     image2.style.display = "none"
+    if( wWidth <= isTablet ){
+      image1l.style.display = "none"
+      image2l.style.display = "none"
+      image3l.style.display = "block"
+      // Imagens no modo portrait
+      image2.style.display = "none"
+      image1.style.display = "none"
+      image3.style.display = "none"
+      
+    }
+    slide1 = false
+    slide2 = false
+    slide3 = true
   }
 }
 
@@ -40,3 +87,64 @@ buttons.forEach((button, i) => {
 
 // Mostra o primeiro slide como padrão
 showSlide(0);
+
+
+window.addEventListener('resize', function(){
+  verifyResizeTech()
+})
+
+verifyResizeTech=()=>{  
+  if( window.innerWidth <= isTablet){
+    if(slide1){
+      image1.style.display = "none"
+      image2.style.display = "none"
+      image3.style.display = "none"
+      image1l.style.display = "block"
+      image2l.style.display = "none"
+      image3l.style.display = "none"
+    }
+    else if(slide2){
+      image1.style.display = "none"
+      image2.style.display = "none"
+      image3.style.display = "none"
+      image1l.style.display = "none"
+      image2l.style.display = "block"
+      image3l.style.display = "none"
+    }
+    if(slide3){
+      image1.style.display = "none"
+      image2.style.display = "none"
+      image3.style.display = "none"
+      image1l.style.display = "none"
+      image2l.style.display = "none"
+      image3l.style.display = "block"
+    }
+  }
+  else { // Seta as imagens para desktop
+    
+    if(slide1){
+      image1.style.display = "block"
+      image2.style.display = "none"
+      image3.style.display = "none"
+      image1l.style.display = "none"
+      image2l.style.display = "none"
+      image3l.style.display = "none"
+    }
+    else if(slide2){
+      image1.style.display = "none"
+      image2.style.display = "block"
+      image3.style.display = "none"
+      image1l.style.display = "none"
+      image2l.style.display = "none"
+      image3l.style.display = "none"
+    }
+    if(slide3){
+      image1.style.display = "none"
+      image2.style.display = "none"
+      image3.style.display = "block"
+      image1l.style.display = "none"
+      image2l.style.display = "none"
+      image3l.style.display = "none"
+    }
+  }
+}
